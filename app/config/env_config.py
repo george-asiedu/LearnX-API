6ee241a9b63fdfb8b53a8c16ff0e63ffc981a214
george-asiedu <asiedug41@gmail.com>
2026-02-08 11:13:50 +0000

    env settings configured

---
+++ app/config/env_config.py
@@ -0,0 +1,25 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from functools import lru_cache
+
+
+class Settings(BaseSettings):
+    ENVIRONMENT: str = 'development'
+    SUPABASE_URL: str
+    SUPABASE_URL: str
+    JWT_KEY: str
+    S3_ACCESS_KEY: str
+    S3_SECRET_KEY: str
+    UPSTASH_REDIS_REST_URL: str
+    UPSTASH_REDIS_REST_TOKEN: str
+    SENDGRID_API_KEY: str
+    SENDER_EMAIL: str
+
+    model_config = SettingsConfigDict(
+        env_file='.env',
+        env_file_encoding='utf-8'
+    )
+
+
+@lru_cache
+def get_settings() -> Settings:
+    return Settings()
