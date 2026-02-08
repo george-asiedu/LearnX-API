54e647974c597412153ac554826a2b5c2c6be46c
george-asiedu <asiedug41@gmail.com>
2026-02-08 14:46:33 +0000

    exception handler implemented

--- app/utils/exceptions.py
+++ app/utils/exceptions.py
@@ -1,6 +1,6 @@
 from typing import Optional, Any
 
-class HTTPException(Exception):
+class APIException(Exception):
     def __init__(self, status_code: int, message: str, detail: Optional[Any] = None):
         self.status_code = status_code
         self.message = message,
