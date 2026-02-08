c7c0b88977f9abd81930bb1d88fa2acd8798b667
george-asiedu <asiedug41@gmail.com>
2026-02-08 14:25:00 +0000

    sql engine defined

--- app/config/supabase.py
+++ app/config/supabase.py
@@ -1,5 +1,6 @@
 from supabase import create_client, Client
 from app.config.env_config import get_settings
+from sqlmodel import create_engine
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,3 +9,5 @@ settings = get_settings()
 url: str = settings.SUPABASE_URL
 key: str = settings.SUPABASE_KEY
 supabase: Client = create_client(url, key)
+
+engine = create_engine(settings.DATABASE_URL, echo=False)
