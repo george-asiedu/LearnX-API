e6907a222dfe5402d9b4abd1806d684d8a029c88
george-asiedu <asiedug41@gmail.com>
2026-02-08 13:26:23 +0000

    messages and allowed origins added

---
+++ app/utils/constants.py
@@ -0,0 +1,12 @@
+messages = {
+    "app_name": "LearnX",
+    "app_description": "LearnX is a LMS and professional networking platform designed to support course delivery, assessments, class engagement, and online collaboration.",
+    "app_version": "1.0.0",
+    "welcome_message": "Welcome to LearnX! Your journey to knowledge starts here.",
+}
+
+origins = [
+    "http://localhost:4200",
+    "http://localhost:8080",
+    "http://localhost:3000",
+]
